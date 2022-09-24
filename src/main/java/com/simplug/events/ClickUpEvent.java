ba59ba0fe9fc8562d0da2738393f3eb3b20a5eb7
alex <ya.ya.zhuck.global@gmail.com>
2022-09-24 21:14:36 +0300

    Работает частично удаление кликаемого айтема

--- src/main/java/com/simplug/events/ClickUpEvent.java
+++ src/main/java/com/simplug/events/ClickUpEvent.java
@@ -30,37 +30,65 @@ public class ClickUpEvent implements Listener {
         }
 
         if (inventoryInteractEvent.getCurrentItem() != null) {
-                Player player = null;
+            Player player = null;
 
-                PlayerData playerData = playerDataService.getByPlayerName(inventoryInteractEvent.getWhoClicked().getName());
+            PlayerData playerData = playerDataService.getByPlayerName(inventoryInteractEvent.getWhoClicked().getName());
 
-                Long count = playerData.getKillCount();
+            Long count = playerData.getKillCount();
+            Long countPig = playerData.getKillCountPig();
+            Long countCow = playerData.getKillCountCow();
 
-                inventoryInteractEvent.getWhoClicked().sendMessage(ChatColor.GOLD + "Клик по айтему");
-                ExpEvent expEvent = new ExpEvent(playerDataService);
+            inventoryInteractEvent.getWhoClicked().sendMessage(ChatColor.GOLD + "Клик по айтему");
+            ExpEvent expEvent = new ExpEvent(playerDataService);
 
-                inventoryInteractEvent.getWhoClicked().sendMessage(ChatColor.BLACK + "" + count);
+            inventoryInteractEvent.getWhoClicked().sendMessage(ChatColor.BLACK + "" + count);
 
-                if (count >= 10 && count < 20 && !inventoryInteractEvent.getCurrentItem().equals(Material.STONE_SWORD)) {
+            if (count >= 100 && countPig >= 50 && countCow >= 50 && inventoryInteractEvent.getCurrentItem().hasItemMeta()) {
 
-                    inventoryInteractEvent.getCurrentItem().subtract();
-                    ItemStack itemStack = new ItemStack(Material.STONE_SWORD);
-                    inventoryInteractEvent.getWhoClicked().getInventory().setItem(1, itemStack);
-                }
-//        } else if (count >= 20) {
-//
-//            inventoryInteractEvent.getWhoClicked().getInventory().removeItem();
-//            ItemStack itemStack = new ItemStack(Material.IRON_SWORD);
-//            inventoryInteractEvent.getWhoClicked().getInventory().setItem(1, itemStack);
-//
-//
-//        }
+                inventoryInteractEvent.getWhoClicked().getInventory().remove(new ItemStack(Material.WOODEN_SWORD));
+//                inventoryInteractEvent.getCurrentItem().subtract();
+                ItemStack itemStack = new ItemStack(Material.STONE_SWORD);
+                inventoryInteractEvent.getWhoClicked().getInventory().setItem(1, itemStack);
+
+            } else if (count >= 200 && countPig >= 100 && countCow >= 100 && inventoryInteractEvent.getCurrentItem().hasItemMeta()) {
+
+                inventoryInteractEvent.getWhoClicked().getInventory().remove(new ItemStack(Material.STONE_SWORD));
+//                inventoryInteractEvent.getCurrentItem().subtract();
+                ItemStack itemStack = new ItemStack(Material.IRON_SWORD);
+                inventoryInteractEvent.getWhoClicked().getInventory().setItem(1, itemStack);
+
+            } else if (count >= 300 && countPig >= 150 && countCow >= 150 && inventoryInteractEvent.getCurrentItem().hasItemMeta()) {
+
+                inventoryInteractEvent.getWhoClicked().getInventory().remove(new ItemStack(Material.IRON_SWORD));
+//                inventoryInteractEvent.getCurrentItem().subtract();
+                ItemStack itemStack = new ItemStack(Material.GOLDEN_SWORD);
+                inventoryInteractEvent.getWhoClicked().getInventory().setItem(1, itemStack);
+
+            } else if (count >= 400 && countPig >= 200 && countCow >= 200 && inventoryInteractEvent.getCurrentItem().hasItemMeta()) {
+
+                inventoryInteractEvent.getWhoClicked().getInventory().remove(new ItemStack(Material.GOLDEN_SWORD));
+//                inventoryInteractEvent.getCurrentItem().subtract();
+                ItemStack itemStack = new ItemStack(Material.DIAMOND_SWORD);
+                inventoryInteractEvent.getWhoClicked().getInventory().setItem(1, itemStack);
             }
         }
+    }
 
     @EventHandler
     public void cancelMove(InventoryMoveItemEvent inventoryMoveItemEvent) {
 
-        inventoryMoveItemEvent.setCancelled(true);
+        if (inventoryMoveItemEvent.getItem().equals(Material.WOODEN_SWORD)) {
+            inventoryMoveItemEvent.setCancelled(false);
+        } else if (inventoryMoveItemEvent.getItem().equals(Material.STONE_SWORD)) {
+            inventoryMoveItemEvent.setCancelled(false);
+        } else if (inventoryMoveItemEvent.getItem().equals(Material.IRON_SWORD)) {
+            inventoryMoveItemEvent.setCancelled(false);
+        } else if (inventoryMoveItemEvent.getItem().equals(Material.GOLDEN_SWORD)) {
+            inventoryMoveItemEvent.setCancelled(false);
+        } else if (inventoryMoveItemEvent.getItem().equals(Material.DIAMOND_SWORD)) {
+            inventoryMoveItemEvent.setCancelled(false);
+        } else {
+            inventoryMoveItemEvent.setCancelled(true);
+        }
     }
 }
